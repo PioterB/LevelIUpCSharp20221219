5241ca5b33cc1b6e6b3c675d729f8714d9bfcfea
Pior Barankiewicz <piotr.barankiewicz@gmail.com>
2022-12-18 21:18:27 +0100

    sandwiches business

---
+++ LevelUpCSharp.Tools/Linq/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LevelUpCSharp.Linq
+{
+	public static class EnumerableExtensions
+	{
+		public static IEnumerable<T> Odd<T>(this IEnumerable<T> source)
+		{
+			return source.Where((item, index) => index % 2 == 0);
+		}
+	}
+}
