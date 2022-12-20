242b282cc1fc86c409bfa836ba17d6c98e155666
Piotr Barankiewicz <PBARANKIEWICZ@beckman.com>
2022-12-20 09:22:50 +0100

    rending maching api

---
+++ LevelUpCSharp.Domain/Retail/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LevelUpCSharp.Retail
+{
+    public interface IVendingMachine<TType, in TKind>: IEnumerable<TType>
+    {
+        void Put(TType item);
+
+        TType Take(TKind kind);
+    }
+}
