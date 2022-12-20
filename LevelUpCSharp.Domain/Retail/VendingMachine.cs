05c841f7c7f626990ec75820d7bc7ebf78716672
Piotr Barankiewicz <PBARANKIEWICZ@beckman.com>
2022-12-20 09:25:05 +0100

    move to files

---
+++ LevelUpCSharp.Domain/Retail/VendingMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LevelUpCSharp.Retail
+{
+    internal class VendingMachine<TType, TKind> : IVendingMachine<TType, TKind>
+    {
+        public IEnumerator<TType> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Put(TType item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TType Take(TKind kind)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
