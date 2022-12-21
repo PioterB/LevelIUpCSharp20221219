ffd5694c4fb0fd5f60cfe0af2a0d17fcdbc7c354
Piotr Barankiewicz <PBARANKIEWICZ@beckman.com>
2022-12-21 13:18:01 +0100

    order respresentation

---
+++ LevelUpCSharp.Domain/Production/ProductionOrder.cs
@@ -0,0 +1,17 @@
+﻿using LevelUpCSharp.Products;
+
+namespace LevelUpCSharp.Production
+{
+    public class ProductionOrder
+    {
+        public ProductionOrder(SandwichKind kind, int count)
+        {
+            Kind = kind;
+            Count = count;
+        }
+
+        public SandwichKind Kind { get; }
+
+        public int Count { get; }
+    }
+}
