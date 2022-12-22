7ca7bb305b9cb6cfb92ad10d2d5b39fa214c9a57
Piotr Barankiewicz <PBARANKIEWICZ@beckman.com>
2022-12-22 09:56:15 +0100

    work broker - fasade for multithreading

---
+++ LevelUpCSharp.Domain/Production/WorkBroker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LevelUpCSharp.Products;
+
+namespace LevelUpCSharp.Production
+{
+    public class WorkBroker : IEmployee
+    {
+        private readonly IEmployee _worker;
+
+        public WorkBroker(IEmployee worker)
+        {
+            _worker = worker;
+        }
+
+        public IEnumerable<Sandwich> Work(ProductionOrder order)
+        {
+            return Enumerable.Range(0, order.Count).AsParallel().SelectMany(index => _worker.Work(new ProductionOrder(order.Kind, 1))).ToArray();
+        }
+    }
+}
